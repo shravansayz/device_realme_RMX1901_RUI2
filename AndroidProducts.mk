516446b3a81ca3be3d05849902e95058983af0b9
Shravan Sayz <shravankumarsayz@gmail.com>
2024-10-01 02:48:40 +0000

    RMX1901: Genesis
    Signed-off-by: Shravan Sayz <shravankumarsayz@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1901.mk
+    $(LOCAL_DIR)/genesis_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1901-userdebug \
-	lineage_RMX1901-eng
+    genesis_RMX1901-userdebug \
+	genesis_RMX1901-eng
